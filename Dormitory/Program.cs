79148cb8f41dd28f21fa271c19c3973284d554c1
Avjol Sakaj <avjolsakaj@outlook.com>
2023-05-06 14:36:40 +0200

    Move to Announcement management class

--- Dormitory/Program.cs
+++ Dormitory/Program.cs
@@ -1,4 +1,4 @@
-﻿using Dormitory.DAL;
+﻿using Dormitory.BLL;
 
 namespace Dormitory;
 
@@ -19,7 +19,7 @@ internal class Program
             switch (choice?.ToUpper())
             {
                 case "AA":
-                    AddAnnouncemnt();
+                    AnnouncementManagement.AddAnnouncemnt();
                     break;
                 case "EA":
                     //TerminateAnnouncemnt();
@@ -35,69 +35,5 @@ internal class Program
                     break;
             }
         }
-    }
-
-    private static void AddAnnouncemnt ()
-    {
-        using var context = new FindRooMateContext();
-
-        Console.WriteLine("Enter announcement title:");
-        var title = Console.ReadLine();
-
-        if (string.IsNullOrEmpty(title))
-        {
-            Console.WriteLine("Title is required.");
-            Console.ReadLine();
-            return;
-        }
-
-        Console.WriteLine("Enter announcement description:");
-        var description = Console.ReadLine();
-
-        if (string.IsNullOrEmpty(description))
-        {
-            Console.WriteLine("Description is required.");
-            Console.ReadLine();
-            return;
-        }
-
-        Console.WriteLine("List of rooms: ");
-        var existingRooms = context.Rooms.ToList();
-        foreach (var room in existingRooms)
-        {
-            Console.WriteLine($"Id: {room.Id}, Room: {room.Name}, Capacity: {room.Capacity}");
-        }
-
-        Console.WriteLine("Enter room Id:");
-        int.TryParse(Console.ReadLine(), out var roomId);
-
-        var roomExists = existingRooms.Any(room => room.Id == roomId);
-        if (!roomExists)
-        {
-            Console.WriteLine($"Room {roomId}, does not exist!");
-            Console.ReadLine();
-            return;
-        }
-
-        var hasActiveAccouncemntForRoom = context.Announcements
-            .Any(x => x.IsActive == true && x.RoomId == roomId);
-
-        if (hasActiveAccouncemntForRoom)
-        {
-            Console.WriteLine("There is already active announcement for this room.");
-            Console.ReadLine();
-            return;
-        }
-
-        _ = context.Announcements.Add(new Announcement
-        {
-            Title = title,
-            Description = description,
-            PublishedDate = DateTime.Now,
-            IsActive = true,
-            RoomId = roomId
-        });
-
-        _ = context.SaveChanges();
     }
 }
