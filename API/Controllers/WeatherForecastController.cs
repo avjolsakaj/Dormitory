a82f733e398376d2e5d90a92401eb55394565293
Avjol Sakaj <avjolsakaj@outlook.com>
2023-06-10 12:04:27 +0200

    DAL

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet("v1/GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get ()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ API/DAL/RestApiDormitoryContext.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.DAL;
+
+public partial class RestApiDormitoryContext : DbContext
+{
+    public RestApiDormitoryContext ()
+    {
+    }
+
+    public RestApiDormitoryContext (DbContextOptions<RestApiDormitoryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Announcement> Announcements { get; set; } = null!;
+    public virtual DbSet<Application> Applications { get; set; } = null!;
+    public virtual DbSet<Dormitory> Dormitories { get; set; } = null!;
+    public virtual DbSet<Room> Rooms { get; set; } = null!;
+    public virtual DbSet<RoomStudent> RoomStudents { get; set; } = null!;
+    public virtual DbSet<Student> Students { get; set; } = null!;
+
+    protected override void OnModelCreating (ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Announcement>(entity =>
+        {
+            entity.Property(e => e.Description).HasMaxLength(50);
+
+            entity.Property(e => e.IsActive)
+                .IsRequired()
+                .HasDefaultValueSql("((1))");
+
+            entity.Property(e => e.PublishedDate).HasColumnType("datetime");
+
+            entity.Property(e => e.Title).HasMaxLength(20);
+
+            entity.HasOne(d => d.Room)
+                .WithMany(p => p.Announcements)
+                .HasForeignKey(d => d.RoomId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Rooms_Announcement");
+        });
+
+        modelBuilder.Entity<Application>(entity =>
+        {
+            entity.HasIndex(e => e.AnnouncementId, "IX_Applications_AnnouncementId");
+
+            entity.HasIndex(e => e.StudentId, "IX_Applications_StudentId");
+
+            entity.Property(e => e.ApplicationDate).HasColumnType("datetime");
+
+            entity.Property(e => e.IsActive)
+                .IsRequired()
+                .HasDefaultValueSql("((1))");
+
+            entity.HasOne(d => d.Announcement)
+                .WithMany(p => p.Applications)
+                .HasForeignKey(d => d.AnnouncementId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Applications_Announcements");
+
+            entity.HasOne(d => d.Student)
+                .WithMany(p => p.Applications)
+                .HasForeignKey(d => d.StudentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Applications_Students");
+        });
+
+        modelBuilder.Entity<Dormitory>(entity =>
+        {
+            entity.Property(e => e.Code).HasMaxLength(20);
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Room>(entity =>
+        {
+            entity.HasIndex(e => e.DormitoryId, "IX_Rooms_DormitoryId");
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+
+            entity.HasOne(d => d.Dormitory)
+                .WithMany(p => p.Rooms)
+                .HasForeignKey(d => d.DormitoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Rooms_Dormitories");
+        });
+
+        modelBuilder.Entity<RoomStudent>(entity =>
+        {
+            entity.ToTable("RoomStudent");
+
+            entity.HasIndex(e => e.RoomId, "IX_RoomStudent_RoomId");
+
+            entity.HasIndex(e => e.StudentId, "IX_RoomStudent_StudentId");
+
+            entity.Property(e => e.EndDate).HasColumnType("date");
+
+            entity.Property(e => e.StartDate).HasColumnType("date");
+
+            entity.HasOne(d => d.Room)
+                .WithMany(p => p.RoomStudents)
+                .HasForeignKey(d => d.RoomId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_RoomStudent_Rooms");
+
+            entity.HasOne(d => d.Student)
+                .WithMany(p => p.RoomStudents)
+                .HasForeignKey(d => d.StudentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_RoomStudent_Students");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Name).HasMaxLength(50);
+
+            entity.Property(e => e.Surname).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial (ModelBuilder modelBuilder);
+}
